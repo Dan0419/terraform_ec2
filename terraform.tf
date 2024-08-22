bbc410231a785f4b27df8c946e92a44201308738
Mohd Danish <mohd.danish0419@gmail.com>
2024-08-22 06:16:36 +0000

    updated ec2 instance name in terraform config file

--- terraform.tf
+++ terraform.tf
@@ -14,10 +14,10 @@ provider "aws" {
 }
 
 #
-resource "aws_instance" "Test_EC2" {
+resource "aws_instance" "Test_EC2_Terraform" {
    ami = "ami-0a0e5d9c7acc336f1"
    instance_type = "t2.micro"
    tags = {
-    	   Name = "my-terra-instance"
+    	   Name = "Test_EC2_Terraform"
   	   }
 }
